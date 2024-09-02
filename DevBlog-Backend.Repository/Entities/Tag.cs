217d12e2f8b9099e17d07740b9c5e4545e747e42
Lasse Hansen <lasse.hansen@danfoss.com>
2024-09-02 14:00:10 +0200

    Created Tag model.

---
+++ DevBlog-Backend.Repository/Entities/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace DevBlog_Backend.Repository.Entities
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Tag(string name)
+        {
+            Id = new();
+            Name = name;
+        }
+    }
+}
