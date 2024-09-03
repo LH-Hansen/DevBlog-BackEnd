07a3204e91009d2cae3ae7bbc66227d76654fba7
Lasse Hansen <lasse.hansen@danfoss.com>
2024-09-03 08:50:47 +0200

    Created Post model class

---
+++ DevBlog-Backend.Repository/Entities/Post.cs
@@ -0,0 +1,28 @@
+﻿using DevBlog_Backend.Repository.Enteties;
+
+namespace DevBlog_Backend.Repository.Entities
+{
+    public class Post
+    {
+        public Guid Id { get; init; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public User Author { get; init; }
+        public List<Tag> Tag { get; set; }
+        public Category Category { get; set; }
+        public List<Comment> Comments { get; set; }
+        public DateTime Timestamp { get; init; }
+
+        public Post(string title, string content, User author, Category category)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Content = content;
+            Author = author;
+            Tag = new List<Tag>();
+            Category = category;
+            Comments = new List<Comment>();
+            Timestamp = DateTime.Now;
+        }
+    }
+}
